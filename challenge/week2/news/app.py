93c9509d12b82a8583526963f38aeb0b07dea712
Alex1996 <13420134410@163.com>
2017-10-29 20:16:09 +0800

    holloe

--- challenge/week2/news/app.py
+++ challenge/week2/news/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import  abort, redirect, url_for
+
+
 app = Flask(__name__)
 #app.run(port=3000)
 
@@ -7,23 +10,41 @@ app = Flask(__name__)
 def index():
     # show the arcticle list    
     # show all the json type file under  his fodler /files   `title`
-    return 'Heloo world'   
+    return "hollo world"
 
 
 
 
-@app.route('/files/<filename>')
-def file(filename):
+@app.route('/files/')
+def file():
     # read filename.json  , and show the content of it 
 
 # if filename ='helloshiyanlou'   show helloshiyanlou.json
 # if filename not exist , return shiyanlou 404 page
-    pass
+#    return render_template('/files/file.html') 
+    return "ok"
+
+#   if filename == 'helloshiyanlou':
+#        return " isOk" 
+
+#   elif filename == 'helloworld':
+#        return "is ok"
+
+ #  else:
+    #something went wrong !
+
+#        return redirect(url_for('File_not_found_page'))
+
+
+
 @app.errorhandler(404)
 def File_not_found_page(error):
     return render_template('404.html'),404
 
-    pass
+    
+
+
+
 
 
 if __name__=='__main__':
