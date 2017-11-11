c3da6fbcd640235f3ae82afcc55e6384901ec37c
Alex1996 <13420134410@163.com>
2017-11-11 15:54:55 +0800

    Hi baby

--- challenge/week4/analysis.py
+++ challenge/week4/analysis.py
@@ -1,12 +1,27 @@
 # need to import json file 
 import json 
 import pandas as pd
+import sys
+
+# exception handler 
+# test if target file exist 
+# @return param : times  and minutes
+
+
 
 def analysis(file, user_id):
 
-    times = 0
-    minutes = 0
+#    times = 0
+#    minutes = 0
 
-    #co
+    try:
+        df = pd.read_json(file)
+    except ValueError:
+        return 0, 0
+
+    df = df[df['user_id'] == user_id].minutes
+
+    return df.count(), df.sum()
+    
 
     return times, minutes
